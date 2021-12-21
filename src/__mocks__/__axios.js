7f177e53b808072d9d7d56b8ab2093e4acb3f464
Umut Akin <umutakin@fastmail.com>
2021-12-21 23:57:52 +0300

    net ninja - react testing library - 13 - mocking requests

---
+++ src/__mocks__/__axios.js
@@ -0,0 +1,22 @@
+// const mockResponse = {
+//   data: {
+//     results: [
+//       {
+//         name: {
+//           first: "Umut",
+//           last: "Akin",
+//         },
+//         picture: {
+//           large: "https://randomuser.me/api/portraits/men/39.jpg",
+//         },
+//         login: {
+//           username: "umutakin",
+//         },
+//       },
+//     ],
+//   },
+// };
+
+// export default {
+//   get: jest.fn().mockResolvedValue(mockResponse),
+// };
