ecfb8b6519754f6289cd14e0b3750284e92b1bdc
Umut Akin <umutakin@fastmail.com>
2021-12-22 00:06:08 +0300

    net ninja - react testing library - 14 - hooks - before and after

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -2,6 +2,31 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 import FollowersList from "../FollowersList";
 
+jest.mock("axios", () => ({
+  __esModule: true,
+
+  default: {
+    get: () => ({
+      data: {
+        results: [
+          {
+            name: {
+              first: "Umut",
+              last: "Akin",
+            },
+            picture: {
+              large: "https://randomuser.me/api/portraits/men/39.jpg",
+            },
+            login: {
+              username: "umutakin",
+            },
+          },
+        ],
+      },
+    }),
+  },
+}));
+
 const MockFollowersList = () => {
   return (
     <BrowserRouter>
@@ -11,14 +36,42 @@ const MockFollowersList = () => {
 };
 
 describe("FollowersList", () => {
+  beforeEach(() => {
+    console.log("RUNNING BEFORE EACH TEST");
+  });
+
+  beforeAll(() => {
+    console.log("RUNNING ONCE BEFORE ALL TESTS");
+  });
+
+  afterEach(() => {
+    console.log("RUNNING AFTER EACH TEST");
+  });
+
+  afterAll(() => {
+    console.log("RUNNING ONCE AFTER ALL TESTS");
+  });
+
   it("should render follower item", async () => {
-    render(<MockFollowersList/>);
+    render(<MockFollowersList />);
     const followerDivElement = await screen.findByTestId("follower-item-0");
     expect(followerDivElement).toBeInTheDocument();
   });
-  it("should render multiple follower items", async () => {
-    render(<MockFollowersList/>);
-    const followerDivElements = await screen.findAllByTestId(/follower-item/i);
-    expect(followerDivElements.length).toBe(5);
+
+  it("should render follower item", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
   });
+
+  it("should render follower item", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+  // it("should render multiple follower items", async () => {
+  //   render(<MockFollowersList />);
+  //   const followerDivElements = await screen.findAllByTestId(/follower-item/i);
+  //   expect(followerDivElements.length).toBe(5);
+  // });
 });
